1fa2babdcfe4c3f46328df9081814dfa9a542dfd
dfong108 <davidsonf108@gmail.com>
2022-06-01 20:43:35 -0400

    starting to work on dynamic create and update

--- src/features/bands/Bands.js
+++ src/features/bands/Bands.js
@@ -1,25 +1,53 @@
-import './style.css';
-import { bands } from '../../seeds';
-import React, { useState, useEffect } from 'react'
-import { useSelector } from 'react-redux';
-import { Link } from 'react-router-dom';
+import "./style.css";
+// import { bands } from "../../seeds";
+import React, { useState, useEffect } from "react";
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import axios from "axios";
 
-import DisplayCard from '../displayCard/DisplayCard';
+import DisplayCard from "../displayCard/DisplayCard";
 
 const Bands = () => {
-  const bands = useSelector(state => state.bands);
+    const [bands, setBands] = useState([]);
 
-  const bandsList = bands?.map((band, i) => (
-    <DisplayCard key={i} type="band" band={band} />
-  ))
+    useEffect(() => {
+        getBands()
+    }, [])
 
-  // console.log(bands)
+  // const bands = useSelector(state => state.bands);
+
+    async function getBands() {
+      try {
+            const url = "http://localhost:4000/bands";
+            await axios.get(url)
+                .then((res) => {
+                    console.log(res.data)
+                    setBands(res.data)
+                })
+    } catch (error) {
+    console.error(error);
+        }
+  };
+
+//   const bandsList = bands?.map((band, i) => (
+//     <DisplayCard key={band._id} type="band" band={band} />
+//   ));
+
+  console.log(bands)
 
   return (
-    <div className='bandsContainer'>
-      {bandsList}
+    <div className="bandsContainer">
+        {
+            bands
+            ? (
+                bands?.map((band, i) => (
+                   <DisplayCard key={band._id} type="band" band={band} />
+                ))
+            ) : <h1>Loading Bands....</h1>
+
+        }
     </div>
   )
-}
+};
 
-export default Bands
+export default Bands;
