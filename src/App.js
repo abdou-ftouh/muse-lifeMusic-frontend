4efae436d2f944311715775c5124091ca9efd853
abdou-ftouh <elftouh13@gmail.com>
2022-06-03 18:33:34 -0400

    Protected our Routes and created a header

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route,Navigate } from 'react-router-dom';
 
 import Header from './features/header/Header';
 import Footer from './features/footer/Footer';
@@ -7,25 +7,51 @@ import HomePage from './pages/Home/HomePage';
 import ShowPage from './pages/Show/ShowPage';
 import FormPage from './pages/Forms/FormPage';
 import Bands from './features/bands/Bands';
+import Login from './pages/Login/Login';
+import Signup from './pages/Signup/Signup';
+import Main from './pages/Main/Main';
+import Events from './features/events/Events';
+import ProtectedRoute from './ProtectedRoute';
 
 function App() {
+  const user= localStorage.getItem("token")
+  console.log(localStorage)
+ 
+console.log(user)   // This is my Access token The important
   return (
     <div className='main'>
-        <Header />
-
+      
+       <Header />
         <Routes>
-          <Route exact path="/" element={<HomePage />} />
+       
+        <Route path='/login' exact element={<Login />}/>
+       <Route path='/' exact element={<Navigate replace to="/login"/>}/>
+  
+       
 
-          <Route exact path="/bands" element={<Bands/>} />
-          <Route exact path="/events" element={<HomePage type="events" />} />
+       <Route path='/signup' exact element={<Signup />}/>
+     
+<Route element={<ProtectedRoute accessToken={user}/>}>
 
-          <Route exact path="/bands/:id" element={<ShowPage id=""/>} />
-          <Route exact path="/events/:id" element={<ShowPage id="" />} />
+<Route exact path="/home" element={<HomePage accessToken={user} />} />
+<Route exact path="/bands" element={<Bands/>} />
+<Route exact path="/bands/:id" element={<ShowPage id=""/>} />
+<Route exact path="/events" element={<Events accessToken={user}/>} />
+<Route exact path="/events/:id" element={<ShowPage id="" />} />
+<Route exact path="/registerband" element={<FormPage />} />
+</Route>
 
-          <Route exact path="/signIn" element={<FormPage type="signIn" />} />
-          <Route exact path="/signUp" element={<FormPage type="signUp" />} />
-          
-          <Route exact path="/admin" element={<FormPage type="signUp" />} />
+        
+
+        
+       
+
+      
+      
+
+         
+         
+       
         </Routes>
 
         <Footer />
